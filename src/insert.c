aee618c1427b18ffad61ab93d884930e0d6a3cbd
Hollywood-Kid <tisinnikita352@gmail.com>
2024-10-31 01:50:40 +0300

    glib insert

--- src/insert.c
+++ src/insert.c
@@ -1,61 +1,69 @@
+#include <glib.h>
+#include <stdio.h>
+#include <string.h>
 #include "../include/insert.h"
 
 void insert(DataBase *db, char *buffer) {
     char *table_name = NULL;
 
-    char *token = strtok(buffer, " ");
-    if (token && strcmp(token, "INSERT") == 0) {
-        token = strtok(NULL, " ");
-        if (token && strcmp(token, "INTO") == 0) {
-            table_name = strtok(NULL, " ");
-        } else {
+    gchar **tokens = g_strsplit(buffer, " ", -1);
+    if (tokens[0] && strcmp(tokens[0], "INSERT") == 0) {
+        if (tokens[1] && strcmp(tokens[1], "INTO") == 0) {
+            table_name = g_strdup(tokens[2]);
+        } 
+        
+        else {
             printf("Invalid command syntax!\n");
+            g_strfreev(tokens);
             return;
         }
-    } else {
+    } 
+    
+    else {
         printf("Invalid command syntax!\n");
+        g_strfreev(tokens);
         return;
     }
 
     Table *table = get_table(db, table_name);
     if (!table) {
         printf("Table '%s' not found!\n", table_name);
+        g_free(table_name);
+        g_strfreev(tokens);
         return;
     }
 
-    token = strtok(NULL, " ");
-    if (token && strcmp(token, "VALUES") == 0) {
-        char *values[table->column_count];
+    if (tokens[3] && strcmp(tokens[3], "VALUES") == 0) {
+        GPtrArray *values = g_ptr_array_new();
 
-        for (size_t i = 0; i < table->column_count; i++) {
-            token = strtok(NULL, ", ");
-            
-            if (token) {
-                values[i] = strdup(token);
-            }
-            
-            else {
-                printf("Not enough values provided for table '%s'.\n", table_name);
-                return;
-            }
+        for (gchar **token = &tokens[4]; *token; token++) {
+            g_ptr_array_add(values, g_strdup(*token));
         }
 
-        if (strtok(NULL, " ") != NULL) {
-            printf("Too many values provided!\n");
+        if (values->len != table->column_count) {
+            printf("Incorrect number of values provided for table '%s'.\n", table_name);
+            g_ptr_array_free(values, TRUE);
+            g_free(table_name);
+            g_strfreev(tokens);
             return;
         }
 
         csv_reader(table, db->name);
-        add_data_to_table(table, values);
+        add_data_to_table(table, (char **)values->pdata);
         csv_write(table, db->name);
 
-        for (size_t i = 0; i < table->column_count; i++) {
-            free(values[i]);
+        for (guint i = 0; i < values->len; i++) {
+            g_free(values->pdata[i]);
         }
+        g_ptr_array_free(values, TRUE);
+
         free_table_data(table);
     } 
     
     else {
         printf("Invalid command syntax, VALUES keyword missing!\n");
     }
+
+    g_free(table_name);
+    g_strfreev(tokens);
 }
